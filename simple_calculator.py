aa3db28b9b2cdb04891963cb068b1961d05f59fb
resisttheflow0-png <resisttheflow0@gmail.com>
2026-05-14 12:15:44 +0300

    updated simple_calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,18 +3,18 @@ numb2 = int(input("Enter the second integer: "))
 
 action = input("Enter the action to perfom: ")
 
-if action == "plus":
-    print(numb1 + numb2)
-elif action == "subtract":
-    print(numb1 - numb2)
-elif action == "multiply":
-    print(numb1 * numb2)
-elif action == "degree":
-    print(numb1 ** numb2) 
-elif action == "divide":
+if action == "plus" or action == "+":
+    print("Result: ", numb1 + numb2)
+elif action == "subtract" or action == "-":
+    print("Result: ", numb1 - numb2)
+elif action == "multiply" or action == "*":
+    print("Result: ", numb1 * numb2)
+elif action == "degree" or action == "**":
+    print("Result: ", numb1 ** numb2) 
+elif action == "divide" or action == "/":
     if numb2 == 0:
         print("Cannot divide by zero")
     else:
-        print(numb1 / numb2)
+        print("Result: ", numb1 / numb2)
 else:
     print("Invalid action")
