3b93d2c28be16aef451e925639e8014c7ffaad84
resisttheflow0-png <resisttheflow0@gmail.com>
2026-05-16 15:01:06 +0300

    first commit

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,20 +1,35 @@
-numb1 = int(input("Enter the first integer: "))
-numb2 = int(input("Enter the second integer: "))
 
-action = input("Enter the action to perfom: ")
 
-if action == "plus" or action == "+":
-    print("Result: ", numb1 + numb2)
-elif action == "subtract" or action == "-":
-    print("Result: ", numb1 - numb2)
-elif action == "multiply" or action == "*":
-    print("Result: ", numb1 * numb2)
-elif action == "degree" or action == "**":
-    print("Result: ", numb1 ** numb2) 
-elif action == "divide" or action == "/":
-    if numb2 == 0:
-        print("Cannot divide by zero")
-    else:
-        print("Result: ", numb1 / numb2)
-else:
-    print("Invalid action")
+
+while True:
+
+        user_input = input("If you want to start the job, type 'go';" \
+            "if you want to stop job, type 'exit': ")
+        if user_input == "go":
+
+            numb1 = int(input("Enter the first integer: "))
+            numb2 = int(input("Enter the second integer: "))
+
+            action = input("Enter the action to perform: ")
+            if action == "plus" or action == "+":
+                print("Result: ", numb1 + numb2)
+            elif action == "subtract" or action == "-":
+                print("Result: ", numb1 - numb2)
+            elif action == "multiply" or action == "*":
+                print("Result: ", numb1 * numb2)
+            elif action == "degree" or action == "**":
+                print("Result: ", numb1 ** numb2) 
+            elif action == "divide" or action == "/":
+                if numb2 == 0:
+                    print("Cannot divide by zero")
+                else:
+                    print("Result: ", numb1 / numb2)
+            else:
+                print("Invalid action")
+
+        elif user_input == "exit":
+            break
+
+        else:
+            print("I didn't quite understand what you meant.")
+
